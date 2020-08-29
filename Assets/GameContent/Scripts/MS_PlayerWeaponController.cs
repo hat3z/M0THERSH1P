15481dee2c6df4a1358557b7da5b36cae3ec82b7
Sebestyén Gergely <htzprdcts@gmail.com>
2020-08-29 16:09:16 +0200

    DevCommit#1.1
    - added basic bulletsystem

---
+++ Assets/GameContent/Scripts/MS_PlayerWeaponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MS_PlayerWeaponController : MonoBehaviour
+{
+
+    public Transform activeWeaponTransform;
+    public GameObject weaponBullerPrefab;
+    public float bulletForce;
+    public Transform bulletPool;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(weaponBullerPrefab, activeWeaponTransform);
+            bullet.transform.SetParent(bulletPool);
+            bullet.transform.localScale = new Vector3(.2f, .2f, .2f);
+            bullet.GetComponent<Rigidbody>().AddForce(-activeWeaponTransform.forward * bulletForce);
+        }
+    }
+}
