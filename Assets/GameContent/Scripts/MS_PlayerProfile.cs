3435888410e32f0f9b4785568d832daed85976c0
Sebestyén Gergely <htzprdcts@gmail.com>
2020-08-31 13:44:13 +0200

    Playerprofile added
    - PlayerProfile: WIP

---
+++ Assets/GameContent/Scripts/MS_PlayerProfile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MS_PlayerProfile
+{
+
+
+    [System.Serializable]
+    public class PlayerShipControl
+    {
+
+    }
+
+    [System.Serializable]
+    public class PlayerWeaponControl
+    {
+
+    }
+
+}
