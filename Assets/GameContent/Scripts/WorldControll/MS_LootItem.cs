7c80a6a759008a59610cd4c78b3d117524fd62d3
Sebestyén Gergely <htzprdcts@gmail.com>
2020-10-20 15:39:12 +0200

    DevCommit#8
    - drop loot (BUG)

---
+++ Assets/GameContent/Scripts/WorldControll/MS_LootItem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MS_LootItem : MonoBehaviour
+{
+
+    public string ItemName;
+
+    Rigidbody rigidBody;
+    float itemDropForce = 20;
+    bool dropCooldown;
+    private void Awake()
+    {
+        if(rigidBody == null)
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(StartDrop());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(!dropCooldown)
+        {
+            StopDrop();
+        }
+    }
+
+    IEnumerator StartDrop()
+    {
+        dropCooldown = true;
+        rigidBody.AddForce(transform.up * itemDropForce, ForceMode.VelocityChange);
+        yield return new WaitForSeconds(2);
+        dropCooldown = false;
+    }
+
+    void StopDrop()
+    {
+        rigidBody.isKinematic = true;
+    }
+
+}
--- Assets/GameContent/Scripts/WorldControll/MS_WorldItem.cs
+++ Assets/GameContent/Scripts/WorldControll/MS_WorldItem.cs
@@ -10,6 +10,7 @@ public class MS_WorldItem : MonoBehaviour
     public int itemHealth;
 
     [Header("Loot settings")]
+    public float LootItemDropForce;
     public List<string> Loot;
     public int maxLootItemCount;
 
@@ -18,10 +19,31 @@ public class MS_WorldItem : MonoBehaviour
     public MS_ParticleLibrary ParticleLibrary;
 
     bool canSpawn = true;
-
+    Collider collider;
+    Transform dropTransform;
+    List<int> dropAngles = new List<int>()
+    {30, 60,90,120, 150,180,210,240,270,300,330};
     private void Start()
     {
         GetLootStartup();
+        SetupColliderByType();
+        dropTransform = transform.GetChild(0).transform;
+    }
+
+    void SetupColliderByType()
+    {
+        if(GetComponent<BoxCollider>())
+        {
+           collider = GetComponent<BoxCollider>();
+        }
+        else if(GetComponent<SphereCollider>())
+        {
+            collider = GetComponent<SphereCollider>();
+        }
+        else if(GetComponent<CapsuleCollider>())
+        {
+            collider = GetComponent<CapsuleCollider>();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -70,6 +92,7 @@ public class MS_WorldItem : MonoBehaviour
         int randomItemIndex;
         for (int i = 0; i < maxLootItemCount; i++)
         {
+
             randomItemIndex = Random.Range(0, ItemDatabase.Instance.GetItemListByTier(Item.itemTier.Normal, ItemType).Count);
             Loot.Add(ItemDatabase.Instance.GetItemListByTier(Item.itemTier.Normal, ItemType)[randomItemIndex].itemName);
         }
@@ -78,19 +101,20 @@ public class MS_WorldItem : MonoBehaviour
     void DropLoot()
     {
         GameObject lootItem;
+        collider.enabled = false;
         for (int i = 0; i < Loot.Count; i++)
         {
             lootItem = Instantiate(ItemDatabase.Instance.GetGameObjectFromItemName(Loot[i]));
+            dropTransform.localEulerAngles = new Vector3(0, 0, Random.Range(0, dropAngles.Count));
             lootItem.transform.SetParent(MS_WorldController.Instance.LootItemPool, true);
             lootItem.transform.position = transform.position;
-            lootItem.GetComponent<Rigidbody>().AddForce(Vector3.forward, ForceMode.Impulse);
         }
     }
 
     IEnumerator DestroyAndDropLoot()
     {
         DropLoot();
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(.5f);
         gameObject.SetActive(false);
     }
 
